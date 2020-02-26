5e0e49a8df86650ea831ed50db998ccd6032db42
strtw <souporstu@gmail.com>
2020-02-26 11:49:11 -0800

    Implement solution to merge the two arrays but 0(n) time and space, but fails on arrays of length 1

--- JavaScript/merge-sorted-arrays.js
+++ JavaScript/merge-sorted-arrays.js
@@ -1,36 +1,25 @@
 //https://www.interviewcake.com/question/javascript/merge-sorted-arrays?course=fc1&section=array-and-string-manipulation
 
 function mergeArray(array1, array2){
-    var mergedArray = [];
-    var shorterArray = array1;
-    var longerArray = array2;
+    var mergedArray = []; 
+    var i = 0;
+    var j = 0;
 
-    if(array1.length !== array2.length){
-        shorterArray = array1.length < array2.length ? array1 : array2;
-        longerArray = array1.length > array2.length ? array1 : array2;
-    }
-
-    for(let i = 0; i < shorterArray.length; i++){
-        if(shorterArray[i] < longerArray[i]){
-            mergedArray.push(shorterArray[i],longerArray[i])
+    while(mergedArray.length < array1.length + array2.length ){
+        if(array1[i] < array2[j]){
+            mergedArray.push(array1[i])
+            i++    
         }else{
-            mergedArray.push(longerArray[i],shorterArray[i])
+            mergedArray.push(array2[j])
+            j++    
         }
     }
-
-    for(let i = shorterArray.length; i < longerArray.length; i++){
-        mergedArray.push(longerArray[i]);
-    }
-
-    mergedArray.sort(function(a,b){
-        return a - b;
-    })
 
     return mergedArray;
 }
 
-const myArray = [3, 4, 6, 10, 11, 15];
-const alicesArray = [1, 5, 8, 12, 14, 19, 20, 25, 30];
+const myArray = [2];
+const alicesArray = [1];
 
 console.log(mergeArray(myArray, alicesArray));
 // logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
