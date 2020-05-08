e0e4c23c431ca1af46189d468d071f5eef83f054
strtw <souporstu@gmail.com>
2020-05-08 14:44:06 -0700

    For the second iteration of the solution I updated the if clauses to reduce complexity, but the general structure remains the same

--- Python/days_between_dates.py
+++ Python/days_between_dates.py
@@ -8,20 +8,17 @@
 ###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
 
 def nextDay(year, month, day):
-    
    # Returns the year, month, day of the next day.
    # Simple version: assume every month has 30 days.
-
-    # YOUR CODE HERE
-    if day + 1 > 30:
+    if day < 30:
+         day = day + 1
+    else:
         day = 1
         month = month + 1
-    else:
-        day = day + 1
     if month > 12:
         month = 1
         year = year + 1
-    return year,month,day
+    return year,month,day 
 
 
 nextDay(1999, 12, 30)
