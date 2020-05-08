06788490fb85294dd2fa1bcfe4d63f8f5d49e4a5
strtw <souporstu@gmail.com>
2020-05-08 14:50:00 -0700

    For the third iteration, I provided multiple return statements to executing eliminate unneccessary code when a day is advanced.

--- Python/days_between_dates.py
+++ Python/days_between_dates.py
@@ -8,19 +8,17 @@
 ###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
 
 def nextDay(year, month, day):
+    
    # Returns the year, month, day of the next day.
    # Simple version: assume every month has 30 days.
+
+    # YOUR CODE HERE
     if day < 30:
-         day = day + 1
+        return year,month, day + 1
     else:
         day = 1
         month = month + 1
     if month > 12:
-        month = 1
-        year = year + 1
-    return year,month,day 
-
-
-nextDay(1999, 12, 30)
-nextDay(2013, 1, 30)
-nextDay(2012, 12, 30)
+        return year + 1, 1, 1
+    else:
+        return year,month,day
