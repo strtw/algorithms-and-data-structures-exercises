67cf7c808f1065331e50f1de074d2e16a425907c
strtw <souporstu@gmail.com>
2020-05-08 15:14:27 -0700

    The next phase of the solution is to define a daysBetweenDates function. My implementation uses a while loop to advance the days using the helper function until the end date is reached.

--- Python/days_between_dates.py
+++ Python/days_between_dates.py
@@ -1,15 +1,25 @@
-###
+
 ### Define a simple nextDay procedure, that assumes
 ### every month has 30 days.
-###
 ### For example:
 ###    nextDay(1999, 12, 30) => (2000, 1, 1)
 ###    nextDay(2013, 1, 30) => (2013, 2, 1)
 ###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
 
+### Define a daysBetweenDates procedure that would produce the 
+### correct results given a correct nextDay procedure
+
+def daysBetweenDates(year1,month1,day1,year2,month2,day2):
+    currentDate = year1,month1,day1 
+    endDate = year2,month2,day2
+    days = 0
+    while(currentDate < endDate): 
+        currentDate = nextDay(*currentDate)
+        days += 1
+    return days
+
+
 def nextDay(year, month, day):
-   # Returns the year, month, day of the next day.
-   # Simple version: assume every month has 30 days
     if day < 30:
         return year,month, day + 1
     else:
@@ -17,3 +27,6 @@ def nextDay(year, month, day):
             return year, month + 1, 1
         else:
             return year + 1,1,1
+
+
+daysBetweenDates(2000,1,1,2000,1,3)
