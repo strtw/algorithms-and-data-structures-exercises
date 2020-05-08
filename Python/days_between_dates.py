4959ecac71d9b2154454a456ffc8039e53c013a7
strtw <souporstu@gmail.com>
2020-05-08 14:53:56 -0700

    For the fourth iteration, I updated the control flow of the function to have contain one main if and else block with sub blocks and I eliminate variable reassignment so that each sub block has its own return statement. This solution is easier to read and more concise

--- Python/days_between_dates.py
+++ Python/days_between_dates.py
@@ -8,17 +8,12 @@
 ###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
 
 def nextDay(year, month, day):
-    
    # Returns the year, month, day of the next day.
-   # Simple version: assume every month has 30 days.
-
-    # YOUR CODE HERE
+   # Simple version: assume every month has 30 days
     if day < 30:
         return year,month, day + 1
     else:
-        day = 1
-        month = month + 1
-    if month > 12:
-        return year + 1, 1, 1
-    else:
-        return year,month,day
+        if month < 12:
+            return year, month + 1, 1
+        else:
+            return year + 1,1,1
