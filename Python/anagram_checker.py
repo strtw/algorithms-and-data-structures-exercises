33315a45b2a9cf3e7d035b4701ec300f84fd3367
strtw <souporstu@gmail.com>
2020-05-15 14:04:17 -0700

    Updated to account for duplicate character. Changed set implementation to dict implementation to create a dict for each string and then compare them for equality

--- Python/anagram_checker.py
+++ Python/anagram_checker.py
@@ -1,4 +1,4 @@
-def anagram_checker(str1, str2):
+def anagram_checker(str1, str2):#0(n) worse case time complexity
     """
     Check if the input strings are anagrams of each other
     don't take into account white space
@@ -7,20 +7,27 @@ def anagram_checker(str1, str2):
     Returns:
        bool: Indicates whether strings are anagrams
     """
- 
-    str1 = str1.replace(" ", "").lower()
-    str2 = str2.replace(" ", "").lower()
-    if len(str1) != len(str2):
+    str1 = str1.replace(" ", "").lower()#0(n)
+    str2 = str2.replace(" ", "").lower()#0(n)
+
+    if(countCharDict(str1) == countCharDict(str2)):#0(n)
+        return True
+    else:
         return False
 
-    str1 = set(str1)
-    str2 = set(str2)#
-    for char in str2: 
-        if(char not in str1):
-            return False
-    return True
+def countCharDict(string):#0(n)
+    stringDict = {}
+    for char in string:#0(n)
+        if( stringDict.get(char)):
+            stringDict[char] = stringDict[char] + 1
+        else:
+            stringDict[char] = 1
+    return stringDict
+
+
 
 print(anagram_checker("cat","tac"))#True
 print(anagram_checker("cato","tac"))#False
 print(anagram_checker('water','waiter'))#False
 print(anagram_checker('Dormitory','Dirty room'))#True
+print(anagram_checker('A gentleman','Elegant men'))#False
