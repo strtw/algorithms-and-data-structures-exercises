db728fdffa5341cbc1a0a4ab3a51ae72a2941025
strtw <souporstu@gmail.com>
2020-05-18 14:00:55 -0700

    For this version, I removed the elif statement which set the tail after the head node had been set and set tail equal to head for the first loop iteration. On subsequent iterations the tail will then be set

--- Python/linked_list_generator.py
+++ Python/linked_list_generator.py
@@ -6,29 +6,24 @@ class Node:
 
 def create_linked_list(input_list):
     head = None
-    tail = None
     for value in input_list:
         if head is None:
             head = Node(value) 
-        elif tail is None:
-            tail = Node(value)
-            head.next = tail
+            tail = head
         else:
         # Move to the tail (the last node)
             tail.next = Node(value)
+            tail = tail.next
     return head
         
 
-linked = create_linked_list([1,2,3])
+linked = create_linked_list('string')
 
 def iterateList(headNode):
-    if(headNode):
       currentNode = headNode
       print(currentNode.value)
       while(currentNode.next is not None):
           print(currentNode.next.value)
           currentNode = currentNode.next
-    else:
-      return None
 
 iterateList(linked)
